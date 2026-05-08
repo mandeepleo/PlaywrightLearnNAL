2e4afaa5f14edae2e0ccab49465a096afba6e1c5
mandeepleo <mandeepleo5@gmail.com>
2026-05-08 19:43:32 +0530

    updated frames.spect.ts with frame tests

--- tests/frames.spec.ts
+++ tests/frames.spec.ts
@@ -1,5 +1,6 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Frame } from "@playwright/test";
 
+// using page.frame() method
 test("frame test", async ({ page }) => {
   await page.goto("https://ui.vision/demo/webtest/frames");
 
@@ -11,4 +12,27 @@ test("frame test", async ({ page }) => {
   } else console.log("Frame not available.");
 
   await page.pause();
+});
+
+// using page.frameLocator() method | nested frames
+test("nested frames using page.frameLocator test", async ({ page }) => {
+  await page.goto("https://ui.vision/demo/webtest/frames");
+  await page.frameLocator("frame[src='frame_3.html']").locator("input[name='mytext3']").fill("Mandeep");
+  // interacting with radio-button in an iframe nested inside a frame
+  await page
+    .frameLocator("frame[src='frame_3.html']") // outer frame
+    .frameLocator("body > center > iframe") // inner (nested) iframe
+    .getByRole("radio", { name: "Hi, I am the UI.Vision IDE" }) // radio-button in the inner iframe
+    .click();
+  await page.pause();
+});
+
+test("name and url of all frames test", async ({ page }) => {
+  await page.goto("https://ui.vision/demo/webtest/frames");
+  let frames: Frame[] = page.frames();
+  console.log("Total no. of frames is: ", frames.length);
+  // printing names of all frames
+  for (let fr of frames) {
+    console.log(fr.name(), ":", fr.url()); // prints name (if available) and url of all frames on page
+  }
 });
