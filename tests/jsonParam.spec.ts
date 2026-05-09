a527adf891c4bb52e470d8381b5c83def485ae3d
mandeepleo <mandeepleo5@gmail.com>
2026-05-09 22:17:48 +0530

    Fix JSON file formatting, enhance test descriptions, and improve type annotations in test files

--- tests/jsonParam.spec.ts
+++ tests/jsonParam.spec.ts
@@ -1,9 +1,41 @@
 import { expect, test } from "@playwright/test";
 import fs from "fs";
+// for the above import statement to work, do the following:
+// Install Node types: npm install -D @types/node
+// Create tsconfig.json if not created earlier and add following code:
+/**
 
-// reading data from JSON
-const jsonPath = "test-data/login-data.json";
-const loginData: any = JSON.parse(fs.readFileSync(jsonPath, "utf-8"));
+  {
+  "compilerOptions": {
+    "target": "ESNext",
+    "module": "CommonJS",    
+    "resolveJsonModule": true,
+    "esModuleInterop": true,
+    "types": ["node"]
+  }
+}
+
+ */
+
+/** reading data from JSON file and converting JSON to Object (Deserialization) **/
+
+// JSON file
+//    ↓
+// readFileSync(..., "utf-8")
+//    ↓
+// String
+//    ↓
+// JSON.parse()
+//    ↓
+// JavaScript Object
+
+/*** Significance of writing 'utf-8': ***/
+// Node.js returns a Buffer object (raw binary data), not a string.
+// But JSON.parse() expects a JSON string, not a Buffer.
+// So, with 'utf-8': Node.js reads the file, decodes bytes using UTF-8 encoding and returns a normal string
+// to JSON.parse method
+
+const loginData: any = JSON.parse(fs.readFileSync("test-data/login-data.json", "utf-8"));
 
 test.describe("Login data driven test", () => {
   for (const data of loginData) {
@@ -13,15 +45,16 @@ test.describe("Login data driven test", () => {
       // Fill login form
       await page.locator("#Email").fill(email);
       await page.locator("#Password").fill(password);
+      await page.waitForTimeout(2_000); // Wait for 2 seconds to simulate user pause
       await page.locator("input[value='Log in']").click();
       if (validity === "valid") {
         // Assert logout link is visible - indicates successful login
         const logoutLink = page.locator("a[href='/logout']");
-        await expect(logoutLink).toBeVisible({ timeout: 5000 });
+        await expect(logoutLink).toBeVisible({ timeout: 5_000 });
       } else {
         // Assert error message is visible
         const errorMessage = page.locator(".validation-summary-errors");
-        await expect(errorMessage).toBeVisible({ timeout: 5000 });
+        await expect(errorMessage).toBeVisible({ timeout: 5_000 });
         // Assert user is still on the login page
         await expect(page).toHaveURL("https://demowebshop.tricentis.com/login");
       }
--- tests/newTab.spec.ts
+++ tests/newTab.spec.ts
@@ -1,17 +1,18 @@
-import { test, expect, Locator } from "@playwright/test";
+import { test, expect, Locator, Page } from "@playwright/test";
 
 test("New Tab Test", async ({ context }) => {
   const page = await context.newPage();
   await page.goto("https://demoqa.com/browser-windows");
   await expect(page).toHaveTitle(/demosite/);
-  const newPagePromise = context.waitForEvent("page"); // not using await keyword because
+  const newPagePromise: Promise<Page> = context.waitForEvent("page"); // not using await keyword because
   // need to execute next statement as the waitForEvent method implicitly keep waiting for event to trigger
   // new tab event is triggered when button is clicked in the following statement
-  await page.getByRole("button", { name: "New Tab" }).click();
-  const newPage = await newPagePromise; // initialize newPage variable with the page object of the newly opened tab
-  await expect(newPage).toHaveURL("https://demoqa.com/sample");
-  const newPageHeading = newPage.getByRole("heading", {
+  await page.getByRole("button", { name: "New Tab" }).click(); // generated page event
+  const newTab: Page = await newPagePromise; // initialize newTab variable with the page object of the newly opened tab
+  await expect(newTab).toHaveURL("https://demoqa.com/sample");
+  const newPageHeading = newTab.getByRole("heading", {
     name: "This is a sample page",
   });
+  await page.waitForTimeout(1_000);
   await expect(newPageHeading).toBeVisible();
 });
