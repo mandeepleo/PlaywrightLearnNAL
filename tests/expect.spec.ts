c74b2641768ed9a8963c93cedff32e3373d61833
mandeepleo <mandeepleo5@gmail.com>
2026-06-15 22:04:29 +0530

    added element color assertion

--- tests/expect.spec.ts
+++ tests/expect.spec.ts
@@ -1,24 +1,27 @@
 import { test, expect } from "@playwright/test";
 
 test("expect method test", async ({ page }) => {
-    await page.goto("https://naveenautomationlabs.com/opencart/ui/elementstate.html");
-    expect(await page.title()).toBe("Register - Playwright Practice");
+  await page.goto("https://naveenautomationlabs.com/opencart/ui/elementstate.html");
+  expect(await page.title()).toBe("Register - Playwright Practice");
 
-    await expect(page.locator("#firstname")).toBeEmpty();
-    await page.locator("#firstname").fill("Mandeep");
-    await expect(page.locator("#firstname")).toHaveValue("Mandeep");
+  // assert element color
+  await expect(page.locator(".logo")).toHaveCSS("color", "rgb(22, 163, 74)");
 
-    await expect(page.locator("#register-btn")).toBeDisabled();
+  await expect(page.locator("#firstname")).toBeEmpty();
+  await page.locator("#firstname").fill("Mandeep");
+  await expect(page.locator("#firstname")).toHaveValue("Mandeep");
 
-    await expect(page.locator("#email")).toBeEditable();
+  await expect(page.locator("#register-btn")).toBeDisabled();
 
-    await expect(page.locator("#username")).not.toBeEditable(); // assert non-editable using not
+  await expect(page.locator("#email")).toBeEditable();
 
-    await expect(page.getByRole("checkbox")).not.toBeChecked();
-    await page.getByRole("checkbox").check();
-    await expect(page.getByRole("checkbox")).toBeChecked();
-    await expect(page.locator("#register-btn")).toBeEnabled();
+  await expect(page.locator("#username")).not.toBeEditable(); // assert non-editable using not
 
-    await page.getByRole("checkbox").uncheck();
-    await expect(page.locator("#register-btn")).toBeDisabled();
+  await expect(page.getByRole("checkbox")).not.toBeChecked();
+  await page.getByRole("checkbox").check();
+  await expect(page.getByRole("checkbox")).toBeChecked();
+  await expect(page.locator("#register-btn")).toBeEnabled();
+
+  await page.getByRole("checkbox").uncheck();
+  await expect(page.locator("#register-btn")).toBeDisabled();
 });
